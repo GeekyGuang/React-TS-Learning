06c6a8ba2d2aadd22e084dfdcb10da013867b6cf
TravisWongX <travis.x.wong@gmail.com>
2021-11-04 15:10:28 +0800

    setState的异步更新

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react'
 import Robot from './components/Robot'
-import robots from './mockdata/robots.json'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 import { ShoppingCart } from './components/ShoppingCart'
 
-function App() {
-  console.log(styles)
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+interface Props {}
+
+interface State {
+  robotGallery: any[]
+  count: number
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      robotGallery: [],
+      count: 0,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ robotGallery: data })
+      })
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+        </div>
+        <button
+          onClick={() => {
+            this.setState((preState, preProps) => {
+              return { count: preState.count + 1 }
+            })
+            this.setState((preState, preProps) => {
+              return { count: preState.count + 1 }
+            })
+            this.setState((preState, preProps) => {
+              return { count: preState.count + 1 }
+            })
+          }}
+        >
+          {this.state.count}
+        </button>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => (
+            <Robot id={r.id} name={r.name} email={r.email} />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((r) => (
-          <Robot id={r.id} name={r.name} email={r.email} />
-        ))}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
